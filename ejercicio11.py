8a52ced7d086a6093a00adec28f687a47787761f
andresrengifo2002 <raulrentellito@gmail.com>
2022-07-19 17:40:06 -0500

    nuevo commit

---
+++ ejercicio11.py
@@ -0,0 +1,10 @@
+from calendar import c
+
+i=0
+if i <= 1:
+     a= print()
+     b= print()
+     c= print()
+     t= (a>b>c)
+
+print ("el mayor es " + t)
---
+++ ejercicio7.py
@@ -0,0 +1,5 @@
+while x<=100:
+    x=2
+    print(x)
+    n=x+2
+    print(n)
